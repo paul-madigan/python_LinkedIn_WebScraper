da1fe538b94910341012b8ec7f3e5d251c6eaefd
paul.madigan <pmadigan25@gmail.com>
2020-02-10 08:53:44 -0600

    Included Looped LinkedIn Company Specific Profile Search and section specific profile scraping

--- LinkedIn_Scraping_Demo.py
+++ LinkedIn_Scraping_Demo.py
@@ -1,60 +1,133 @@
 from selenium import webdriver
-
-local_chromedriver_exe = "C:\\Users\\paul.madigan\\Selenium\\ChromeDriver\\chromedriver.exe"
-driver = webdriver.Chrome(local_chromedriver_exe)
-
-driver.get("https://www.linkedin.com/in/patrickwylie/")
-# driver.get("https://www.linkedin.com/in/anthony-prado-207b5599/")
-
-input("Press Enter to continue...")
-
-""" About """
-abuotText = driver.find_element_by_xpath("//h2[text()='About']/../../p").text
-print("About: ", abuotText)
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import csv
+import time
+import random
 
 
-
-""" Experience """
-jobTitleText = driver.find_element_by_xpath("//h2[text()='Experience']/../..//h3").text
-print("Job Title: ", jobTitleText)
-
-companyText = driver.find_element_by_xpath("//h2[text()='Experience']/../..//h4").text
-print("Company: ", companyText)
-
-lengthofServiceText = driver.find_element_by_xpath("//h2[text()='Experience']/../..//p").text
-print("Length of Service: ", lengthofServiceText)
-
-locationText = driver.find_element_by_xpath("//h2[text()='Experience']/../..//p[@class='experience-item__location experience-item__meta-item']").text
-print("Location: ", locationText)
-
-jobDescriptionText = driver.find_element_by_xpath("//h2[text()='Experience']/../..//p[@class='show-more-less-text__text--less']").text
-print("Job Description: ", jobDescriptionText)
+""" Initialize CSV """
+# Classification - Experience -> 1
+# Anything Else -> 0
+with open('LI_Block_Data.csv', 'a', newline='') as block_file, open('LI_Sentence_Data.csv', 'a', newline='') as sentence_file:
+# with open('LI_Block_Data.csv', 'w', newline='') as block_file, open('LI_Sentence_Data.csv', 'w', newline='') as sentence_file:
+    block_writer = csv.writer(block_file)
+    sentence_writer = csv.writer(sentence_file)
 
 
+    """ Use Chrome profile in new Chrome window """
+    options = Options()
+    options.add_argument('--user-data-dir=C:/Users/paul.madigan/AppData/Local/Google/Chrome/User Data/Profile 1')
+    options.add_argument("start-maximized")
+    local_chromedriver_exe = "C:\\Users\\paul.madigan\\Selenium\\ChromeDriver\\chromedriver.exe"
 
-""" Education """
-schoolText = driver.find_element_by_xpath("//h2[text()='Education']/../..//h3[@class='result-card__title']").text
-print("School: ", schoolText)
+    driver = webdriver.Chrome(executable_path=local_chromedriver_exe, chrome_options=options)
+    original_window = driver.current_window_handle  # Store the ID of the original window
+    url = "https://www.linkedin.com/company/accenture/people/?keywords="
 
-degreeText = driver.find_element_by_xpath("//h2[text()='Education']/../..//h4").text
-print("Degree: ", degreeText)
+    letterlistA = ["a", "e", "i", "o", "u"]
+    letterlistB = ["j", "h", "g", "f", "d", "a", "e", "i", "o", "u", "l", "r", "t", "p", "k"]
+    for letter in letterlistB:
+        searchkey = letter + random.choice(letterlistA)
+        driver.get(url + searchkey)
+        time.sleep(5)
 
-yearsText = driver.find_element_by_xpath("//h2[text()='Education']/../..//p").text
-print("Years: ", yearsText)
+        """ Search for users """
+        # peopleSearchBox = driver.find_element_by_xpath("//input[@id='people-search-keywords']")
+        # peopleSearchBox.send_keys('b')
+        # peopleSearchBox.send_keys(Keys.ENTER)
 
+        x = 0
+        while x < 5:
+            driver.find_element_by_xpath("//html[1]").send_keys(Keys.END)
+            time.sleep(10)
+            x = x+1
 
+        time.sleep(3)
+        totalTileNumber = len(driver.find_elements_by_xpath("//ul[@class='org-people-profiles-module__profile-list']/li"))
+        print(totalTileNumber)
+        for tileNumber in range(1, (totalTileNumber + 1)):
+            try:
+                # Get the person's tile and open it in a new chrome tab
+                personTile = driver.find_element_by_xpath(
+                    "//li[@class='org-people-profiles-module__profile-item'][{}]//a[@data-control-name='people_profile_card_name_link']".format(
+                        tileNumber))
+                builder = ActionChains(driver)
+                builder.move_to_element(personTile).perform()
+                link_to_click = personTile.send_keys(Keys.CONTROL + Keys.RETURN)
+                time.sleep(3)
 
-""" Licenses & Certifications """
-certificationNameText = driver.find_element_by_xpath("//h2[text()='Licenses & Certifications']/../..//h3").text
-print("Certification Name: ", certificationNameText)
+                # Once the new tab opens, save the current tab ID and switch windows to the new tab
+                driver.switch_to.window(driver.window_handles[1])
 
-issuingBodyText = driver.find_element_by_xpath("//h2[text()='Licenses & Certifications']/../..//h4").text
-print("Issuing Body: ", issuingBodyText)
+                """ Get Experience """
+                totalExperienceInstances = len(driver.find_elements_by_xpath("//h2[text()='Experience']/../../ul/li"))
+                for experienceInstance in range(1, (totalExperienceInstances + 1)):
+                    if driver.find_element_by_xpath(
+                            "//h2[text()='Experience']/../..//li[{}]".format(experienceInstance)).is_displayed():
+                        try:
+                            experienceText = driver.find_element_by_xpath(
+                                "//h2[text()='Experience']/../..//li[{}]//div[@class='pv-entity__extra-details ember-view']".format(
+                                    experienceInstance)).text
+                            expEditText = experienceText.replace("=", "").replace("-", "").replace(",", "").replace("…",
+                                                                                                                    "").replace(
+                                "see more", "").replace("\n", " ").strip().replace("•", ".").replace("·", ".")
+                            block_writer.writerow([expEditText, "1"])
+                            print(expEditText)
+                            # split the text up by period
+                            splitTextList = expEditText.split('.')
+                            for line in splitTextList:
+                                full_line = line + "."  # Adding in period
+                                sentence_writer.writerow([full_line, "1"])
+                        except Exception as e:
+                            print("Unable to find experience text. \n", e)
 
-issueDateText = driver.find_element_by_xpath("//h2[text()='Licenses & Certifications']/../..//span").text
-print("Issue Date: ", issueDateText)
+                """ Get Education """
+                totalEducationInstances = len(driver.find_elements_by_xpath("//h2[text()='Education']/../../ul/li"))
+                for educationInstance in range(1, (totalEducationInstances + 1)):
+                    if driver.find_element_by_xpath(
+                            "//h2[text()='Education']/../..//li[{}]".format(educationInstance)).is_displayed():
+                        try:
+                            educationText = driver.find_element_by_xpath(
+                                "//h2[text()='Education']/../..//li[{}]".format(educationInstance)).text
+                            # uniName = driver.find_element_by_xpath("//h2[text()='Education']/../..//li[{}]//h3".format(educationInstance)).text
+                            # degreeName = driver.find_element_by_xpath("//h2[text()='Education']/../..//li[{}]//span[@class='pv-entity__comma-item']".format(educationInstance)).text
+                            eduEditText = educationText.replace("=", "").replace("-", " ").replace(",", "").replace("…",
+                                                                                                                    "").replace(
+                                "see more", "").replace("\n", " ").strip().replace("Degree Name", " ").replace(
+                                "Field Of Study", " ").replace("Dates attended or expected graduation", " ").replace(
+                                "Dates attended or expected graduation", " ").replace("Activities and Societies:",
+                                                                                      " ").replace("•", ".").replace("·",
+                                                                                                                     ".")
+                            block_writer.writerow([eduEditText, "0"])
+                            # sentence_writer.writerow([full_line, "0"])
+                        except Exception as e:
+                            print("Unable to find education text. \n", e)
 
-credentialIDText = driver.find_element_by_xpath("//h2[text()='Licenses & Certifications']/../..//div[@class='certifications__credential-id']").text
-print("Credentials ID: ", credentialIDText)
+                """ Get Licenses and Certifications """
+                totalLicCertInstances = len(
+                    driver.find_elements_by_xpath("//h2[text()='Licenses & Certifications']/../../ul/li"))
+                for LicCertInstance in range(1, (totalLicCertInstances + 1)):
+                    if driver.find_element_by_xpath("//h2[text()='Licenses & Certifications']/../..//li[{}]".format(
+                            LicCertInstance)).is_displayed():
+                        try:
+                            LicCertText = driver.find_element_by_xpath(
+                                "//h2[text()='Licenses & Certifications']/../..//li[{}]".format(LicCertInstance)).text
+                            LicCertEditText = LicCertText.replace("=", "").replace("-", " ").replace(",", " ").replace("…", "").replace(
+                                "see more", "").replace("\n", " ").strip().replace("Issuing authority", " ").replace(
+                                " Issued date and  if applicable  expiration date of the certification or license",
+                                " ").replace("•", ".").replace("·", ".").replace("No Expiration Date", "")
+                            block_writer.writerow([LicCertEditText, "0"])
+                            # sentence_writer.writerow([full_line, "0"])
+                        except Exception as e:
+                            print("Unable to find licenses and Certifications text. \n", e)
+
+                # Close current tab and switch to original
+                driver.close()
+                driver.switch_to.window(original_window)  # Switch back to original tab
+                time.sleep(2)
+            except Exception as e:
+                print("Unable to find tile, Moving to next. \n", e)
 
 
